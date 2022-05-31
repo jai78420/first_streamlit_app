ebe682eb1aed60e2b7d3c9542404b0c632c623a1
Janardhan Reddy <106464547+jai78420@users.noreply.github.com>
2022-05-31 12:46:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥪Omega 3 & Blueberry Oatmeala')
 streamlit.text('🍣Kale, Spinach & Rocket Smoothie')
