3908d39e5cb8221186a03a54dbc8976ca9a9d2a0
Janardhan Reddy <106464547+jai78420@users.noreply.github.com>
2022-05-31 12:43:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 streamlit.title('My Parents New Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeala')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥪Omega 3 & Blueberry Oatmeala')
+streamlit.text('🍣Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥯Hard-Boiled Free-Range Egg')
